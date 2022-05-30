83f0fcca0a61f2226428c42d1a85703c05639298
DanielitaGa <danielagarciache1999@gmail.com>
2022-05-30 18:24:59 -0500

    Activitys commit

--- app/src/main/java/com/nadershamma/apps/examen_espejo_garcia/MainActivity2ESGA.java
+++ app/src/main/java/com/nadershamma/apps/examen_espejo_garcia/MainActivity2ESGA.java
@@ -3,12 +3,18 @@ package com.nadershamma.apps.examen_espejo_garcia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2ESGA extends AppCompatActivity {
 
+    private Button btn_Siguiente;
+    private Button btn_Cerrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2_esga);
+
+        btn_Siguiente = findViewById(R.id.buttonIngresar);
+        btn_Cerrar = findViewById(R.id.buttonCerrar);
     }
 }
