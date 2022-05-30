105e1c49d04972fa9c3e0a4233ad9e6b941560b3
Bryan <bespejo7032@uta.edu.ec>
2022-05-30 18:24:49 -0500

    Lay 1

--- app/src/main/java/com/nadershamma/apps/examen_espejo_garcia/MainActivity1ESGA.java
+++ app/src/main/java/com/nadershamma/apps/examen_espejo_garcia/MainActivity1ESGA.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity1ESGA extends AppCompatActivity {
+
+    ArrayList<Integer> indices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity1_esga);
+        indices = new ArrayList<>();
     }
 }
